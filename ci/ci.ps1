c1f8db60a4112a6173f8f3866cafede73b983a2b
Pedro Reglero <pedropablo@ciclosmontecastelo.com>
2022-06-16 00:58:31 +0200

    commit

--- ci/ci.ps1
+++ ci/ci.ps1
@@ -1,3 +1,3 @@
 cd ..
-docker build -t pedroreglero/pisoapp-backend:v1.0.16 .
-docker push pedroreglero/pisoapp-backend:v1.0.16
+docker build -t pedroreglero/pisoapp-backend:v1.0.17 .
+docker push pedroreglero/pisoapp-backend:v1.0.17
